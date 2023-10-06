713f168dd6fa5a25c10bbbc289e7d57b606e7eba
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-10-06 23:54:15 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Reverse alternate nodes in Link List/reverse-alternate-nodes-in-link-list.cpp
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+/* A linked list node */
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+struct Node *start = NULL;
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+    
+}
+
+void insert()
+{
+    int n,value;
+    cin>>n;
+    struct Node *temp;
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            start = new Node(value);
+            temp = start;
+            continue;
+        }
+        else
+        {
+            temp->next = new Node(value);
+            temp = temp->next;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+/*
+  reverse alternate nodes and append at the end
+  The input list will have at least one element  
+  Node is defined as 
+  struct Node
+  {
+      int data;
+      struct Node *next;
+    
+      Node(int x){
+        data = x;
+        next = NULL;
+      }
+    
+   };
+
+*/
+class Solution
+{
+    public:
+    void rearrange(struct Node *odd)
+    {
+        //add code here
+        // 06.10.23 POTD
+        
+        Node* nd = nullptr;
+        Node *od = odd;
+        
+        while(od and od->next){
+            // Node *nxt = od->next;
+            Node *nw = new Node(od->next->data);
+            nw->next = nd;
+            nd = nw;
+            
+            if(od->next->next){
+                od->next = od->next->next;
+                od = od->next;
+            }
+            else
+                break;
+        }
+        
+        od->next = nd;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--) {
+        insert();
+        Solution ob;
+        ob.rearrange(start);
+        printList(start);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
