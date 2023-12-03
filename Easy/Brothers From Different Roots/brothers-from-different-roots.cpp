63bb52d5bbedec59f3cab563e55a45ef132acb55
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-12-03 19:23:39 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Brothers From Different Roots/brothers-from-different-roots.cpp
@@ -0,0 +1,154 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*Structure of the Node of the BST is as
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    int countPairs(Node* root1, Node* root2, int x)
+    {
+        // 03.12.23 POTD
+        int ans = 0;
+        unordered_map<int,int> u1, u2;
+        dfs(root1, u1);
+        dfs(root2, u2);
+        
+        for(auto p: u1){
+            if(p.first <= x && u2.find(x-p.first) != u2.end()){
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+    
+    void dfs(Node* root, unordered_map<int,int>& u){
+        if(!root)   return;
+        
+        dfs(root->left, u);
+        u[root->data]++;
+        dfs(root->right, u);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        string tree1, tree2;
+        getline(cin, tree1);
+        getline(cin, tree2);
+        Node* root1 = buildTree(tree1);
+        Node* root2 = buildTree(tree2);
+        int x;
+        cin >> x;
+        cin.ignore();
+        Solution ob;
+        cout << ob.countPairs(root1, root2, x) << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
