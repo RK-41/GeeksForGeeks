3cb66c962a19a2b84ce7318de2eefe026adf1333
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-02-19 21:22:33 +0530

    Updated Solution - GfG to GitHub

--- Medium/Game with String/game-with-string.cpp
+++ Medium/Game with String/game-with-string.cpp
@@ -18,14 +18,20 @@ public:
         priority_queue<int> q;
         unordered_map<char,int> count;
         
+        // Counting each character's frequency
         for(auto c: s){
             count[c]++;
         }
         
+        // Storing the frequency count in a priority so that 
+        //  frequencie counts from higher end will be easily accessible
         for(auto p: count){
             q.push(p.second);
         }
         
+        // Taking the highest frequency count 't', 
+        //  and pushing the updated frequency count 't-1'
+        // This operation is done 'k' times
         while(k--){
             int t=q.top();
             q.pop();
@@ -33,12 +39,16 @@ public:
             q.push(t-1);
         }
         
+        // Remaining queue elements are the frequency counts after removal of 'k' character,
+        //  which are used to calculate the required sum
         while(q.size()){
             int t=q.top();
             q.pop();
+            
             ans += (t*t);
         }
         
+        // Returning the required sum
         return ans;
     }
 };
