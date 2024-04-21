c90ceabec4ce371c6899252488e576813df4f089
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-04-21 23:13:48 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Three way partitioning/three-way-partitioning.cpp
@@ -0,0 +1,143 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    //Function to partition the array around the range such 
+    //that array is divided into three parts.
+    void threeWayPartition(vector<int>& arr,int a, int b)
+    {
+        // code here 
+        // 21.04.24 POTD
+        int i=0, j=0, n=arr.size();
+        
+        while(i<n && j<n){
+            if(arr[j] < a){
+                swap(arr[i], arr[j]);
+                i++;
+            }
+            j++;
+        }
+        // for(auto e:arr){
+        //     cout<<e<<" ";
+        // }
+        // cout<<endl;
+        
+        // while(i<n && (arr[i]==a || arr[i]<=b)){
+        //     i++;
+        // }
+        // for(auto e:arr){
+        //     cout<<e<<" ";
+        // }
+        // cout<<endl;
+        
+        j=n-1;
+        while(i<n && j>i){
+            while(arr[i]<=b){
+                i++;
+            }
+            while(arr[j]>b){
+                j--;
+            }
+            if(i<j && arr[j] <= b){
+                swap(arr[i], arr[j]);
+                i++;
+            }
+            // cout<<i<<j<<" ";
+            // j--;
+        }
+        // cout<<endl;
+        
+        // for(auto e:arr){
+        //     cout<<e<<" ";
+        // }
+        // cout<<endl;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        
+        vector<int> array(N);
+        unordered_map<int,int> ump;
+        
+        for(int i=0;i<N;i++){
+            cin>>array[i];
+            ump[array[i]]++;
+        }
+        
+        int a,b;
+        cin>>a>>b;
+        
+        vector<int> original = array;
+
+        int k1=0,k2=0,k3=0;
+        int kk1=0;int kk2=0;int kk3=0;
+        
+        for(int i=0; i<N; i++)
+        {
+            if(original[i]>b)
+                k3++;
+            else if(original[i]<=b and original[i]>=a)
+                k2++;
+            else if(original[i]<b)
+                k1++;
+        }
+        
+        Solution ob;
+        ob.threeWayPartition(array,a,b);
+      
+        for(int i=0;i<k1;i++)
+        {
+            if(array[i]<b)
+            kk1++;
+        }
+        
+        for(int i=k1;i<k1+k2;i++)
+        {
+            if(array[i]<=b and array[i]>=a)
+            kk2++;
+            
+        }
+        
+        for(int i=k1+k2;i<k1+k2+k3;i++)
+        {
+            if(array[i]>b)
+            kk3++;
+        }
+        bool ok = 0;
+        if(k1==kk1 and k2 ==kk2 and k3 == kk3)
+            ok = 1;
+        
+        for(int i=0;i<array.size();i++)
+            ump[array[i]]--;
+        
+        for(int i=0;i<array.size();i++)
+            if(ump[array[i]]!=0)
+                ok=0;
+        
+        if(ok)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+        
+    }
+    
+    return 0;
+}
+
+// } Driver Code Ends
