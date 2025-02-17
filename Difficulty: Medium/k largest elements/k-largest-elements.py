899dfc02c4d90c84bb2fd1e110a3a4f3a219fd91
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2025-02-17 19:58:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/k largest elements/k-largest-elements.py
@@ -0,0 +1,26 @@
+class Solution:
+	def kLargest(self, arr, k):
+		# Your code here
+		# 17.02.25 POTD
+		n = len(arr)
+		arr.sort()
+		
+		return reversed(arr[n-k:])
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        arr = list(map(int, input().strip().split()))
+        k = int(input().strip())
+        ob = Solution()
+        ans = ob.kLargest(arr, k)
+
+        print(" ".join(map(str, ans)))
+        tc -= 1
+        print("~")
+
+# } Driver Code Ends
