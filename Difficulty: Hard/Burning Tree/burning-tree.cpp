2370831864f9de33b7c12d5ee88b683f41575b18
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2025-05-20 22:08:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Burning Tree/burning-tree.cpp
+++ Difficulty: Hard/Burning Tree/burning-tree.cpp
@@ -1,89 +1,6 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    Node *left;
-    Node *right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-
-
-Node *buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node *root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node *> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node *currNode = queue.front();
-        queue.pop();
-
-        // Get the current Node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current Node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current Node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-
-// } Driver Code Ends
-//User function Template for C++
-
 /*
-struct Node {
+class Node {
+  public:
     int data;
     Node *left;
     Node *right;
@@ -96,84 +13,45 @@ struct Node {
 */
 class Solution {
   public:
-    int minTime(Node* root, int target) 
-    {
-        // Your code goes here
-        // 20.08.24 potd
-        int ans=0;
-        unordered_map<int,vector<int>> adj;
-        unordered_map<int,int> vis;
-        queue<Node*> q;
-        queue<pair<int,int>> pq;
-        
-        q.push(root);
-        while(q.size()){
-            int sz = q.size();
-            
-            while(sz--){
-                Node* curr = q.front();
-                q.pop();
-                
-                if(curr->left){
-                    adj[curr->data].push_back(curr->left->data);
-                    adj[curr->left->data].push_back(curr->data);
-                    q.push(curr->left);
-                }
-                
-                if(curr->right){
-                    adj[curr->data].push_back(curr->right->data);
-                    adj[curr->right->data].push_back(curr->data);
-                    q.push(curr->right);
-                }
+    int minTime(Node* root, int target) {
+        // code here
+        // 20.05.25 potd
+        deque<pair<Node*, int>> q = {{root, -1}};
+        unordered_map<Node*, Node*> parent;
+        parent[root] = nullptr;
+        Node* tar = nullptr;
+        Node* node = nullptr;
+        while(!q.empty()){
+            node = q.front().first;
+            q.pop_front();
+            if(node->data == target)tar = node;
+            if(node->left){
+                q.push_back({node->left, -1});
+                parent[node->left] = node;
+            }
+            if(node->right){
+                q.push_back({node->right, -1});
+                parent[node->right] = node;
             }
         }
-        
-        pq.push({target,0});
-        vis[target] = 1;
-        
-        while(pq.size()){
-            pair<int,int> p = pq.front();
-            pq.pop();
-            
-            ans = max(ans, p.second);
-            
-            for(int j: adj[p.first]){
-                if(!vis[j]){
-                    pq.push({j, p.second+1});
-                    vis[j] = 1;
-                }   
+        int t = 0, tt;
+        q.push_back({tar, 0});
+        while(!q.empty()){
+            node = q.front().first;
+            tt = q.front().second;
+            t = max(t, tt);
+            q.pop_front();
+            node->data = 0;
+            if(node->left && node->left->data){
+                q.push_back({node->left, tt + 1});
+            }
+            if(node->right && node->right->data){
+                q.push_back({node->right, tt + 1});
+            }
+            if(parent[node] && parent[node]->data){
+                q.push_back({parent[node], tt + 1});
             }
         }
-        
-        return ans;
+        return t;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() 
-{
-    int tc;
-    scanf("%d ", &tc);
-    while (tc--) 
-    {    
-        string treeString;
-        getline(cin, treeString);
-        // cout<<treeString<<"\n";
-        int target;
-        cin>>target;
-        // cout<<target<<"\n";
-
-        Node *root = buildTree(treeString);
-        Solution obj;
-        cout<<obj.minTime(root, target)<<"\n"; 
-
-        cin.ignore();
-
-    }
-
-
-    return 0;
-}
-
-// } Driver Code Ends
