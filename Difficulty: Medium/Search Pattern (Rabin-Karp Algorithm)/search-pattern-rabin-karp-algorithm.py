c927bb680d7bb72afaf40924c6a5d9bbe0248020
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2025-06-06 23:25:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search(self, pat, txt):
+        # 06.06.25 potd
+        start = 0
+        res = []
+        while True:
+            pos = txt.find(pat, start)
+            if pos == -1:
+                break
+            res.append(pos + 1)  # 1-based index
+            start = pos + 1
+        return res
