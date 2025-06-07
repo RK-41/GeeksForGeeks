b1ac1a1e49ad9d5a547a29c6fddc4f02a1b6e62a
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2025-06-07 23:17:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Span in two Binary Arrays/longest-span-in-two-binary-arrays.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonSum(self, a1, a2):
+        #Code here.
+        # 07.06.25 potd
+        d = {0: -1}
+        m = s = 0
+        
+        for i in range(len(a1)):
+            s += a1[i] - a2[i]
+            
+            if s in d:
+                m = max(m, i-d[s])
+            else:
+                d[s] = i
+        
+        return m
