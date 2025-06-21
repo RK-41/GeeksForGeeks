d3b6100d30bfd499e9cbb21afac04f6a4367b162
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2025-06-21 21:33:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.py
@@ -0,0 +1,18 @@
+from collections import deque
+class Solution:
+    def catchThieves(self, arr, k):
+        #code  here
+        # 21.06.25 potd
+        police = deque([i for i,person in enumerate(arr) if person=='P'])
+        theives = deque([i for i,person in enumerate(arr) if person=='T'])
+        count = 0
+        while len(police) and len(theives):
+            if abs(theives[0]-police[0]) <= k:
+                count += 1
+                theives.popleft()
+                police.popleft()
+            elif theives[0] < police[0]:
+                theives.popleft()
+            else:
+                police.popleft()
+        return count
