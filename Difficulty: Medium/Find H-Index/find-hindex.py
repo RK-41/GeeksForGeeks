4b93cc8b8b620e5c6dfe42afc130a379161526ef
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2025-08-18 23:52:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.py
+++ Difficulty: Medium/Find H-Index/find-hindex.py
@@ -3,7 +3,7 @@ class Solution:
     # Function to find hIndex
     def hIndex(self, citations):
         #code here
-        # 06.12.24 potd
+        # 18.08.25 (POTD)
         citations.sort(reverse=True)
         hIdx = 0
         
@@ -16,21 +16,3 @@ class Solution:
         return hIdx
 
 
-#{ 
- # Driver Code Starts
-# Initial Template for Python 3
-
-# Main
-if __name__ == '__main__':
-    t = int(input())
-    while t > 0:
-        t -= 1
-        arr = [int(x) for x in input().strip().split()]
-
-        ob = Solution()
-        result = ob.hIndex(arr)
-
-        print(result)
-        print("~")
-
-# } Driver Code Ends
