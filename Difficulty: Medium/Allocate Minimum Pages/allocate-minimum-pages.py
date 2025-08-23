0dbcbdd1d3c78fe8c81e9f28cdee292a3d4b1765
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2025-08-23 23:54:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.py
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.py
@@ -1,55 +1,37 @@
 class Solution:
-    
     #Function to find minimum number of pages
     def findPages(self, arr, k):
-        # 18.12.24 potd
+        # 23.12.24 (POTD)
         if len(arr) < k:
             return -1
             
-        l, h = max(arr), sum(arr)
+        leastMaxPages, maxPages = max(arr), sum(arr)
         
-        while l <= h:
-            mid = (l+h)//2
-            students = self.countStudents(arr, mid)
+        while leastMaxPages <= maxPages:
+            currPages = (leastMaxPages + maxPages)//2
+            students = self.countStudents(arr, currPages)
             
             if students > k:
-                l = mid + 1
+                leastMaxPages = currPages + 1
             else:
-                h = mid - 1
+                maxPages = currPages - 1
                 
-        return l
+        return leastMaxPages
 
+    #Function to count no. of students having at least 'pages' pages
     def countStudents(self, arr, pages):
         students = 1
-        pagesStudent = 0
+        pagesForCurrStudent = 0
         n = len(arr)
         
         for i in range(n):
-            if pagesStudent + arr[i] <= pages:
-                pagesStudent += arr[i]
+            if pagesForCurrStudent + arr[i] <= pages:
+                pagesForCurrStudent += arr[i]
             else:
                 students += 1
-                pagesStudent = arr[i]
+                pagesForCurrStudent = arr[i]
         
         return students
         
 
 
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-import bisect
-#Main
-if __name__ == '__main__':
-    t = int(input())
-    while t:
-        t -= 1
-        A = [int(x) for x in input().strip().split()]
-        nd = [int(x) for x in input().strip().split()]
-        D = nd[0]
-        ob = Solution()
-        ans = ob.findPages(A, D)
-        print(ans)
-        print("~")
-
-# } Driver Code Ends
