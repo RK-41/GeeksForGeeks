38fc2a000a8439a4643fd7e9bee0f25e2d9409ee
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2025-08-31 23:45:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Container With Most Water/container-with-most-water.py
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.py
@@ -2,45 +2,29 @@
 class Solution:
     def maxWater(self, arr):
         # code here
-        # 13.01.25 POTD
+        # 18.01.25 (POTD)
         ans, left, right = 0, 0, len(arr)-1
 
+        # Looping through the array using two pointers
         while left < right:
-            ans = max(ans, (right-left)*min(arr[left],arr[right]))
+            # Calculating amount of water (area = width * height)
+            water = (right-left) * min(arr[left],arr[right])
+            ans = max(ans, water)
             
+            # Closing down from the end with shorter line
             if arr[left] < arr[right]:
                 left += 1
             elif arr[left] > arr[right]:
                 right -= 1
             else:
+                # Going for next taller line if both of the current 
+                #   lines are equal
                 if arr[left+1] > arr[right-1]:
                     left += 1
                 else:
                     right -= 1
-                    
+        
+        # Returning max amount of water contained            
         return ans
 
 
-#{ 
- # Driver Code Starts
-#Initial template for Python 3
-
-import math
-
-
-def main():
-    t = int(input())
-    while (t > 0):
-
-        arr = [int(x) for x in input().strip().split()]
-        obj = Solution()
-        print(obj.maxWater(arr))
-
-        t -= 1
-        print("~")
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
