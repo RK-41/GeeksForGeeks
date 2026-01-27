aa7567697d11a9aded1ff20858794049b97f3e1c
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2026-01-27 22:45:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Word Search/word-search.cpp
+++ Difficulty: Medium/Word Search/word-search.cpp
@@ -1,73 +1,48 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
+#define vvb vector<vector<bool>>
+#define vvc vector<vector<char>>
 class Solution {
   public:
-    int d[5] = {0, 1, 0, -1, 0};
-    bool isWordExist(vector<vector<char>>& mat, string& word) {
-        // Code here
-        // 01.02.25 POtD
+    int d[5] = {0, 1, 0, -1, 0};    // Direction 'vector'
+    bool isWordExist(vvc& mat, string& word) {
+        // 06.02.25
+        // 27.01.26 (POTD)
         int n = mat.size(), m = mat[0].size();
         
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
+                // When current char matches with first char of given word
                 if(mat[i][j] == word[0]){
-                    vector<vector<bool>> vis(n, vector<bool>(m, false));
+                    // A m*n boolean vector to store visit status of chars
+                    vvb vis(n, vector<bool>(m, false));
+                    
+                    // Checking if given word can be formed
                     if(check(mat, i, j, 0, word, vis))
                         return true;
                 }
             }
         }
         
-        return false;
+        return false;   // When given word can't be formed
     }
     
 private:
-    bool check(vector<vector<char>>& mat, int i, int j, int idx, string& word, vector<vector<bool>>& vis){
-        if(idx == word.size())
+    bool check(vvc& mat, int i, int j, int idx, string& word, vvb& vis){
+        if(idx == word.size())  // Meaning all the chars matched with word
             return true;
 
-        if(i<0 || j<0 || i>=mat.size() || j>=mat[0].size() || vis[i][j] || mat[i][j] != word[idx])
+        // Out of bound, already visited or doesn't match with current word char
+        if(i<0 || j<0 || i>=mat.size() || j>=mat[0].size() 
+            || vis[i][j] || mat[i][j] != word[idx])
             return false;
             
-        vis[i][j] = true;
+        vis[i][j] = true;   // Setting visit status
         
-        for(int x=0; x<4; x++){
-            bool res = check(mat, i+d[x], j+d[x+1], idx+1, word, vis);
-            if(res) return true;
+        for(int x=0; x<4; x++){ // Checking in all four directions
+            if(check(mat, i+d[x], j+d[x+1], idx+1, word, vis))
+                return true;
         }
         
-        vis[i][j] = false;
-        return false;
+        vis[i][j] = false;  // Resetting visit status
+        return false;   // This path doesn't form the word
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<char>> mat(n, vector<char>(m, '*'));
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                cin >> mat[i][j];
-        string word;
-        cin >> word;
-        Solution obj;
-        bool ans = obj.isWordExist(mat, word);
-        if (ans)
-            cout << "true\n";
-        else
-            cout << "false\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
