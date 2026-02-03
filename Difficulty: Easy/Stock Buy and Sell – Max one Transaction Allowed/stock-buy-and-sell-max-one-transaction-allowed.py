0bd67bee469e07067475b915e9dca209970d35c2
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2026-02-03 23:59:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.py
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.py
@@ -1,35 +1,22 @@
 class Solution:
-    def maximumProfit(self, prices):
+    def maxProfit(self, prices):
         # code here
-        # 22.11.24 POTD
+        # 03.02.26 (potd)
         ans, n = 0, len(prices)
-        mxLTR = [0]*n
+        mxPriceRTL = [0]*n
         mx = prices[-1]
         
         # Find max element up to pos i from right to left
         for i in range(n-1,0,-1):
-            if mx < prices[i]:
-                mx = prices[i]
-            mxLTR[i] = mx
+            mx = max(mx, prices[i])
+            mxPriceRTL[i] = mx
             
-        # Find diff b/w curr price and max price to it's right
+        # Find max profit
         for i in range(n-1):
-            ans = max(ans, mxLTR[i+1] - prices[i])
+            # Diff b/w curr price and max price to its right
+            diff = mxPriceRTL[i+1] - prices[i]
+            ans = max(ans, diff)
         
         return ans
 
 
-#{ 
- # Driver Code Starts
-if __name__ == "__main__":
-    t = int(input())  # Read number of test cases
-    for _ in range(t):
-        # Read input and split it into a list of integers
-        prices = list(map(int, input().split()))
-        # Create a Solution object and calculate the result
-        obj = Solution()
-        result = obj.maximumProfit(prices)
-        # Print the result
-        print(result)
-
-# } Driver Code Ends
